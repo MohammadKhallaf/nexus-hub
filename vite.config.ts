5c3afff2af288467081152882c8ed08e521fa909
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-10-08 20:12:41 +0300

    configs: update configurations after pwa init integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
@@ -19,6 +19,35 @@ export default defineConfig({
           floatPrecision: 2,
         },
         icon: true,
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "nexus-hub",
+        short_name: "nexus-hub",
+        description: "nexus-hub",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
       },
     }),
   ],
